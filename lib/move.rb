3809fbab07d5eb28d2e56d713a36e961a1f03b84
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-03-01 23:22:13 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(user_input)
 end
 
 def move (board, user_input, players_character = "X" )
-  players_character = (user_input) 
+  players_character = board(user_input)
   puts "#{players_character}"
 end
